6b609d3ffc66bba94cb72d1ab571a921c193355b
Michell Algarra Barros <michell.algarra@gmail.com>
2024-05-03 11:50:03 -0300

    feat(lib/components): added an index ts

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,1 +1,13 @@
 // Export all components
+
+// Icons
+export * from './icons/Logo.svelte';
+export * from './icons/Chevron.svelte';
+
+// Memphis
+export * from './memphis/MemphiDashedCircle.svelte';
+export * from './memphis/MemphiTwoLineDots.svelte';
+
+// Layout
+export * from './layout/Drawer.svelte';
+export * from './layout/Header.svelte';
