c0cc9829b5549795bd77af389931b3542f9b5c99
Michell Algarra Barros <michell.algarra@gmail.com>
2024-05-02 21:57:19 -0300

    build: setup the project base

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import '../app.scss';
+	import type { Snippet } from 'svelte';
+
+	let { children }: { children: Snippet } = $props();
+</script>
+
+<main class="bg-primary">
+	{@render children()}
+</main>
+
+<style lang="scss">
+	main {
+		width: 100vw;
+		height: 100vh;
+
+		box-sizing: border-box;
+
+		& {
+			box-sizing: border-box;
+		}
+	}
+</style>
