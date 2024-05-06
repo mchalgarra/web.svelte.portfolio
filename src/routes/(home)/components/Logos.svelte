b20d4a00c89f154434d65084919ef7f23c090a93
Michell Algarra Barros <michell.algarra@gmail.com>
2024-05-06 20:52:56 -0300

    fix(Logos): fixed overflow

--- src/routes/(home)/components/Logos.svelte
+++ src/routes/(home)/components/Logos.svelte
@@ -10,33 +10,11 @@
 
 <svelte:window bind:innerWidth />
 
-<div class="logos relative flex items-center whitespace-nowrap w-full h-32">
-	<div class="slider flex items-center justify-center gap-24 sm:gap-6 sm:w-full h-full p-12 sm:p-6">
-		<div class="logo h-[22px]">
-			<Micro1 className="text-light w-full h-full" accentColor="var(--pfl-accent)" />
-		</div>
-
-		<div class="logo h-[22px]">
-			<Cadmus className="text-light w-full h-full" accentColor="var(--pfl-accent)" />
-		</div>
-
-		<div class="logo h-7">
-			<Google className="text-light w-full h-full" mono={true} />
-		</div>
-
-		<div class="logo h-7">
-			<Lg className="text-light w-full h-full" mono={true} />
-		</div>
-
-		<div class="logo h-7">
-			<Facens className="text-light w-full h-full" accentColor="var(--pfl-accent)" />
-		</div>
-	</div>
-
-	<!-- Carrousel duplication -->
-
-	{#if innerWidth < 660}
-		<div class="slider flex items-center justify-center gap-24 sm:gap-6 h-full p-12 sm:p-6">
+<div class="logos relative w-full h-32">
+	<div class="wrapper flex items-center whitespace-nowrap w-full h-full overflow-hidden">
+		<div
+			class="slider flex items-center justify-center gap-24 sm:gap-6 sm:w-full h-full p-12 sm:p-6"
+		>
 			<div class="logo h-[22px]">
 				<Micro1 className="text-light w-full h-full" accentColor="var(--pfl-accent)" />
 			</div>
@@ -57,7 +35,33 @@
 				<Facens className="text-light w-full h-full" accentColor="var(--pfl-accent)" />
 			</div>
 		</div>
-	{/if}
+
+		<!-- Carrousel duplication -->
+
+		{#if innerWidth < 660}
+			<div class="slider flex items-center justify-center gap-24 sm:gap-6 h-full p-12 sm:p-6">
+				<div class="logo h-[22px]">
+					<Micro1 className="text-light w-full h-full" accentColor="var(--pfl-accent)" />
+				</div>
+
+				<div class="logo h-[22px]">
+					<Cadmus className="text-light w-full h-full" accentColor="var(--pfl-accent)" />
+				</div>
+
+				<div class="logo h-7">
+					<Google className="text-light w-full h-full" mono={true} />
+				</div>
+
+				<div class="logo h-7">
+					<Lg className="text-light w-full h-full" mono={true} />
+				</div>
+
+				<div class="logo h-7">
+					<Facens className="text-light w-full h-full" accentColor="var(--pfl-accent)" />
+				</div>
+			</div>
+		{/if}
+	</div>
 
 	<div
 		class="background absolute top-0 left-1/2 -translate-x-1/2 bg-black opacity-30 w-screen h-full z-0"
@@ -65,7 +69,7 @@
 </div>
 
 <style lang="scss">
-	.logos:hover > .slider {
+	.logos:hover .slider {
 		animation-play-state: paused;
 	}
 
