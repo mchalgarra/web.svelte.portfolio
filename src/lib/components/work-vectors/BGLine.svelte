b0d2d5e79913157cd60d13b787ab5f32bcd03a7d
Michell Algarra Barros <michell.algarra@gmail.com>
2025-12-11 18:03:37 -0300

    feat: added some assets

---
+++ src/lib/components/work-vectors/BGLine.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	let { className = '' } = $props();
+</script>
+
+<svg
+	class="pointer-events-none {className}"
+	width="1920"
+	height="3407"
+	viewBox="0 0 1920 3407"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<g filter="url(#filter0_d_48_6304)">
+		<path
+			d="M2037 3346.63C2037 3346.63 1785.89 3184.67 1607.5 3120.63C1094.89 2936.61 400.264 3726.66 216 3214.13C121.033 2949.99 364.195 2785.79 340 2506.13C318.019 2252.07 107.536 2140.32 133.5 1886.63C194.009 1295.41 1220.16 2033.95 1628.5 1602.13C1798.49 1422.37 1602.31 1223.14 1628.5 982.633C1652.06 766.279 1777.31 651.964 1742.5 437.133C1650.77 -128.986 829.652 -26.4171 296 183.633C97.4209 261.796 -164 478.633 -164 478.633"
+			stroke="currentColor"
+		/>
+	</g>
+	<defs>
+		<filter
+			id="filter0_d_48_6304"
+			x="-180.316"
+			y="0"
+			width="2233.58"
+			height="3406.38"
+			filterUnits="userSpaceOnUse"
+			color-interpolation-filters="sRGB"
+		>
+			<feFlood flood-opacity="0" result="BackgroundImageFix" />
+			<feColorMatrix
+				in="SourceAlpha"
+				type="matrix"
+				values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+				result="hardAlpha"
+			/>
+			<feOffset />
+			<feGaussianBlur stdDeviation="8" />
+			<feComposite in2="hardAlpha" operator="out" />
+			<feColorMatrix
+				type="matrix"
+				values="0 0 0 0 0.6514 0 0 0 0 0.835 0 0 0 0 0.565 0 0 0 0.7 0"
+			/>
+			<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_48_6304" />
+			<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_48_6304" result="shape" />
+		</filter>
+	</defs>
+</svg>
